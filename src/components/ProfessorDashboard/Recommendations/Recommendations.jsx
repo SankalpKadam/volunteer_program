bfbe08794a22f952fc28612cd9188a522fa819ea
SankalpKadam <sankalptp07@gmail.com>
2024-03-03 11:19:48 -0600

    Update Recommendations.jsx

--- src/components/ProfessorDashboard/Recommendations/Recommendations.jsx
+++ src/components/ProfessorDashboard/Recommendations/Recommendations.jsx
@@ -3,9 +3,23 @@ import Navbar from '../../universalComponents/Navbar/Navbar'
 import './Recommendations.css'
 const Recommendations = () => {
     const [selectedValue, setSelectedValue] = useState("Click to select student");
+    const [professorName, setProfessorName] = useState("");
+    const [recommendationText, setRecommendationText] = useState("");
+
     const dropDownref = useRef();
     function toggleDropDown(params) {
         dropDownref.current.classList.toggle("showDropDown")
+    }
+    function generateLetter(e) {
+        e.preventDefault();
+        if (selectedValue!="Click to select student" && professorName && recommendationText) {
+            setSelectedValue("Click to select student")
+            setProfessorName("")
+            setRecommendationText("")
+        }
+        else{
+            alert("Fill all the details");
+        }
     }
     const menuItems = [
         {
@@ -13,12 +27,16 @@ const Recommendations = () => {
             link: "/professorhome"
         },
         {
-            text: "Tasks",
-            link: "/professorhome/tasks"
+            text: "Recommendations",
+            link: "/professorhome/recommend"
         },
         {
             text: "Reports",
             link: "/professorhome/report"
+        },
+        {
+            text:"Tasks",
+            link:"/professorhome/tasks"
         },
         {
             text: "Logout",
@@ -36,22 +54,22 @@ const Recommendations = () => {
                 <form className="recommendation__form">
                     <label htmlFor="" className='recommendation__label'>
                         <span className='recommendation__inputSpan'>Professor Name</span>
-                        <input type="text" name="" pattern="^[A-Za-z\s]*$" id="name" className='recommendation__input' />
+                        <input type="text" name="" pattern="^[A-Za-z\s]*$" id="name" className='recommendation__input' value={professorName} onChange={(e)=>setProfessorName(e.target.value)}/>
                     </label>
                     <label htmlFor="" className='recommendation__label'>
-                        <span className='recommendation__inputSpan'>Task Details</span>
-                        <textarea name="taskDetails" id="taskDetails" ></textarea>
+                        <span className='recommendation__inputSpan'>Recommendation Text</span>
+                        <textarea name="taskDetails" id="taskDetails" value={recommendationText} onChange={(e)=>setRecommendationText(e.target.value)}></textarea>
                     </label>
                     <label htmlFor="" className='recommendation__label'>
                         <span className='submit__spanStyle'>Recommendation For</span>
-                        <input type="text" id="proftaskmanagementdate" className='proftaskmanagement__input' placeholder='Click to Select student' onClick={toggleDropDown} value={selectedValue} />
+                        <input type="text" id="proftaskmanagementdate" className='proftaskmanagement__input' placeholder='Click to Select student' onClick={toggleDropDown} value={selectedValue} onChange={(e)=>setSelectedValue(e.target.value)}/>
                         <div className="dropdown" ref={dropDownref}>
                             {
                                 studentList.map((student, index) => <p className='dropdown__item' key={index} id={student} onClick={() => { setSelectedValue(student); toggleDropDown() }}>{student}</p>)
                             }
                         </div>
                     </label>
-                    <button className='submitreport__submitBtn'>Generate Letter</button>
+                    <button className='submitreport__submitBtn' onClick={generateLetter}>Generate Letter</button>
 
                 </form>
             </div>
