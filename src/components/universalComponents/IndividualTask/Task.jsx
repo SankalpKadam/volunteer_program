d30ffc0863abe8b4a1bd01e9627bd65877286786
SankalpKadam <sankalptp07@gmail.com>
2024-03-03 00:17:39 -0600

    updated task

--- src/components/universalComponents/IndividualTask/Task.jsx
+++ src/components/universalComponents/IndividualTask/Task.jsx
@@ -5,7 +5,9 @@ const Task = ({title, description, priority, deadline}) => {
   return (
     <Link className='task' to={"detailed"}>
             <div className='task__row1'>
-                <div className="task__title">{title} <span className={`task__${priority} task__priority`}>{priority}</span></div>
+                <div className="task__title">{title}
+                {/* <span style={{"marginLeft":"1rem","fontSize":"var(--text-sm)","fontWeight":"normal"}}>Priority:</span>  */}
+                <span className={`task__${priority} task__priority`}>{priority}</span></div>
                 <div className="task__secondaryDetail">{deadline}</div>
                 <button className='task__completedBtn'>Mark as Completed</button>
             </div>
