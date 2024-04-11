d42dbb2a0798989145584e11a47e69406bde6f46
SankalpKadam <sankalptp07@gmail.com>
2024-04-11 10:17:56 -0500

    created graduates and professors models and tables

---
+++ backend/database/migrations/2024_04_11_145630_create_graduates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('graduates', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_name');
+            $table->date('student_graduation_date');
+            $table->string('student_email');
+            $table->string('student_password');
+            $table->string('student_phone_number');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('graduates');
+    }
+};
