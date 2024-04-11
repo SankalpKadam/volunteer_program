237739eeb1a8e1df4e2630b4f6ef8b057776e395
SankalpKadam <sankalptp07@gmail.com>
2024-04-11 12:50:06 -0500

    Api setup done and created the register new user api

--- backend/app/Http/Controllers/GraduatesController.php
+++ backend/app/Http/Controllers/GraduatesController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Graduates;
 class GraduatesController extends Controller
 {
     //
     public function register(Request $request) {
-        \Log::info(json_encode($request-> all()));
-        return 'Hello';
+        \Log::info(json_encode($request->all()));
+        $graduate = new Graduates;
+        $graduate->student_name = $request->input('name');
+        $graduate->student_graduation_date = $request->input('date');
+        $graduate->student_email = $request->input('email');
+        $graduate->student_password = $request->input('password');
+        $graduate->student_phone_number = $request->input('number');
+        $graduate->save();
+        return response()->json([
+            'status'=>200,
+            'message'=>"User Registered Successfully"
+        ]);
     }
 }
