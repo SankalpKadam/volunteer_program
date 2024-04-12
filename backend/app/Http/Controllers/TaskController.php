7f9f3986c8c58f5d3d7694785ae9ee5a24135d32
SankalpKadam <sankalptp07@gmail.com>
2024-04-12 00:27:05 -0500

    setting up tasks table and api

---
+++ backend/app/Http/Controllers/TaskController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tasks;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function getStudentTasks(Request $request){
+        \Log::info(json_encode($request->all()));
+        $tasks = Tasks::where('graduate_id',$request->input('id'))->get();
+        return response()->json([
+            'status'=>200,
+            'tasks'=>$tasks
+        ]);
+    }
+    public function saveTask(Request $request){
+        \Log::info(json_encode($request->all()));
+        $tasks = new Tasks;
+        $tasks->task_title = $request->input('title');
+        $tasks->task_description = $request->input('description');
+        $tasks->task_deadline = $request->input('deadline');
+        $tasks->task_start = now();
+        $tasks->task_graduate_id = 1;
+        $tasks->task_professor_id = 1;
+        $tasks->save();
+        return response()->json([
+            'status'=>200,
+            'message'=>'Task saved successfully'
+        ]);
+
+    }
+}
---
+++ backend/database/migrations/2024_04_12_050400_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('task_title');
+            $table->string('task_description');
+            $table->date('task_deadline');
+            $table->date('task_startdate');
+            $table->foreignId('graduate_id')->constrained()->onDelete('cascade');
+            $table->foreignId('professor_id')->constrained()->onDelete('cascade');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
