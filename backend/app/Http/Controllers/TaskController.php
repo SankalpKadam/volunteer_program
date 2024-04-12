e4421ac28b3ede82b3b26db22e4a0a038edc99e5
SankalpKadam <sankalptp07@gmail.com>
2024-04-12 17:44:03 -0500

    Update TaskController.php

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -15,6 +15,16 @@ class TaskController extends Controller
             'tasks'=>$tasks
         ]);
     }
+
+    public function getProfessorTasks(Request $request){
+        \Log::info(json_encode($request->all()));
+        $tasks = Tasks::where('professor_id',$request->input('id'))->get();
+        return response()->json([
+            'status'=>200,
+            'tasks'=>$tasks
+        ]);
+    }
+
     public function saveTask(Request $request){
         \Log::info(json_encode($request->all()));
         $tasks = new Tasks;
@@ -23,12 +33,34 @@ class TaskController extends Controller
         $tasks->task_deadline = $request->input('deadline');
         $tasks->task_startdate = now();
         $tasks->graduate_id = $request->input('graduate_id');
-        $tasks->professor_id = 1;
+        $tasks->professor_id = $request->input('professor_id');
+        $tasks->task_status = 0;
+        $tasks->task_priority = $request->input('priority');
         $tasks->save();
         return response()->json([
             'status'=>200,
             'message'=>'Task saved successfully'
         ]);
 
+    }
+
+    public function updatestatus(Request $request){
+        \Log::info(json_encode($request->all()));
+        $task = Tasks::find($request->input('id'));
+        $task->task_status=1;
+        $task->save();
+        return response()->json([
+            'status'=>200,
+            'message'=>'Task status updated successfully'
+        ]);
+    }
+
+    public function getTask(Request $request){
+        \Log::info(json_encode($request->all()));
+        $task = Tasks::find($request->input('id'));
+        return response()->json([
+            'status'=>200,
+            'task'=>$task
+        ]);
     }
 }
