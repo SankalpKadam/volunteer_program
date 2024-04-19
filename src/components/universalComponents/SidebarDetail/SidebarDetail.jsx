4012b2cddbd43623f9b76fb95132d7449d9b649b
SankalpKadam <sankalptp07@gmail.com>
2024-04-19 00:26:00 -0500

    Update SidebarDetail.jsx

--- src/components/universalComponents/SidebarDetail/SidebarDetail.jsx
+++ src/components/universalComponents/SidebarDetail/SidebarDetail.jsx
@@ -2,24 +2,44 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './SidebarDetail.css'
 import { useDispatch } from 'react-redux'
-import { setCurrentReport } from '../../../features/tasks/taskSlice'
+import { clearMessages, setCurrentReport, setMessages } from '../../../features/tasks/taskSlice'
 import axios from 'axios'
-const SidebarDetail = ({ title, secondaryDetail, description, link, id }) => {
+const SidebarDetail = ({ title, secondaryDetail, description, link, id, report }) => {
     const dispatch = useDispatch();
+    const getSingleReport = () => {
+        axios.get(process.env.REACT_APP_API_URL + '/getsinglereport', {
+            params: {
+                'id': id
+            }
+        }).then((response) => {
+
+            dispatch(setCurrentReport({ ...response.data.reports[0] }))
+        })
+    }
+    const getMessagesForUser = () => {
+        axios.get(process.env.REACT_APP_CHAT_API_URL + 'getmessages', {
+            params: {
+                id: id
+            }
+        }).then((response) => {
+            if (response.status == 200) {
+                dispatch(clearMessages())
+                response.data.map((message) =>{dispatch(setMessages({message }))})
+            }
+        }).catch((err) => console.log(err))
+    }
+    const decider = () => {
+        if (report) {
+            getSingleReport()
+        } else {
+            getMessagesForUser()
+        }
+    }
     return (
         <Link className='sidebardetail' to={link}>
             <div className='sidebardetail__row1'>
                 <div className="sidebardetail__title">{title}</div>
-                <div className="sidebardetail__secondaryDetail" onClick={()=>{
-                axios.get(process.env.REACT_APP_API_URL+'/getsinglereport',{
-                    params:{
-                        'id':id
-                    }
-                }).then((response)=>{
-
-                    dispatch(setCurrentReport({...response.data.reports[0]}))
-                })
-        }}>{secondaryDetail? secondaryDetail:"View"}</div>
+                <div className="sidebardetail__secondaryDetail" onClick={decider}>{secondaryDetail ? secondaryDetail : "View"}</div>
             </div>
             {
                 description &&
